ef88ae2a9ed435ba540a3e0943286f994256e32d
Maxim <mhuryanov@gmail.com>
2018-03-09 14:09:32 -0400

    change

--- application/libraries/BaseController.php
+++ application/libraries/BaseController.php
@@ -1,12 +1,6 @@
 <?php defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' ); 
 
-/**
- * Class : BaseController
- * Base Class to control over all the classes
- * @author : Kishor Mali
- * @version : 1.1
- * @since : 15 November 2016
- */
+
 class BaseController extends CI_Controller {
 	protected $role = '';
 	protected $vendorId = '';

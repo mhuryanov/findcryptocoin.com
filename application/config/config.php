063dac1f830ad9ce9b052e0c4d60f43c04376cbf
Maxim <mhuryanov@gmail.com>
2018-03-25 22:57:25 -0300

    my com

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,7 @@
 | environments.
 |
 */
-$base  = "https://".$_SERVER['HTTP_HOST'];
+$base  = "http://".$_SERVER['HTTP_HOST'];
 $base .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 $config['base_url'] = $base;
 
